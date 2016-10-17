9262507414d1e422462752941fe91af399833f4b
Steve Prentice <prentice@cisco.com>
2016-10-17 12:31:58 -0700

    Import Wx2ConfigAdapter.

--- server/src/main/java/com/ciscospark/helloworld/HelloWorldApplication.java
+++ server/src/main/java/com/ciscospark/helloworld/HelloWorldApplication.java
@@ -1,11 +1,14 @@
 package com.ciscospark.helloworld;
 
+import com.ciscospark.server.Wx2ConfigAdapter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(Wx2ConfigAdapter.class)
 public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @Override
