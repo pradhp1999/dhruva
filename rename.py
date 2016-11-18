9df056d604f5a65542720f5d3c4cea7c10bf7ee5
Steve Prentice <prentice@cisco.com>
2016-11-18 14:34:58 -0800

    rename.py script.

---
+++ rename.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+"""Usage: rename.py <service-name>"""
+from __future__ import print_function
+import os.path
+import inflection
+from docopt import docopt
+from subprocess import check_call, check_output
+
+def rename(name):
+    ldashed_name = name.lower()
+    humanized_name = inflection.titleize(name)
+    camel_name = inflection.camelize(inflection.underscore(name))
+    all_lower_name = ldashed_name.replace('-', '')
+
+    print("Dashed name:", ldashed_name)
+    print("Human name: ", humanized_name)
+    print("Camel name: ", camel_name)
+    print("Package:    ", all_lower_name)
+
+    files = [x for x in check_output('git ls-files', shell=True).splitlines() if x != 'rename.py']
+    for path in files:
+        new_path = path.replace('hello-world', ldashed_name)
+        new_path = new_path.replace('HelloWorld', camel_name)
+        new_path = new_path.replace('helloworld', all_lower_name)
+        if new_path != path:
+            check_call("mkdir -p {}".format(os.path.dirname(new_path)), shell=True)
+            check_call("git mv {} {}".format(path, new_path), shell=True)
+        print(new_path)
+        with open(new_path) as f:
+            contents = f.read()
+        contents = contents.replace('hello-world', ldashed_name)
+        contents = contents.replace('Hello World', humanized_name)
+        contents = contents.replace('HelloWorld', camel_name)
+        contents = contents.replace('helloworld', all_lower_name)
+        with open(new_path, 'w') as f:
+            f.write(contents)
+        
+
+if __name__ == '__main__':
+    args = docopt(__doc__)
+    rename(args.get('<service-name>'))
+
