ec1ed393755dca4c09ba3188c3d3a3137a42e5fd
Steve Prentice <prentice@cisco.com>
2017-01-21 13:58:05 -0800

    Disable WebMvcAutoConfiguration.
    Can't use WebMvcAutoConfiguration because it overrides the web mvc configuration
    performed by AbstractConfig and things like wx2-core's ExceptionResolver isn't
    configured.

--- server/src/main/java/com/ciscospark/helloworld/HelloWorldApplication.java
+++ server/src/main/java/com/ciscospark/helloworld/HelloWorldApplication.java
@@ -1,11 +1,14 @@
 package com.ciscospark.helloworld;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = WebMvcAutoConfiguration.class)
 public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @Override
